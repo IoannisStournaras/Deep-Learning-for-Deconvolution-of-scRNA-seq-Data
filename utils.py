87635bc20a2b54fda835c4961a218605ac3ddbb5
John <giannistou@hotmail.com>
2019-06-20 13:15:25 +0100

    Updating utils

---
+++ utils.py
@@ -0,0 +1,60 @@
+import random
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import numpy as np
+from sklearn.utils.linear_assignment_ import linear_assignment
+
+def Permute_Dataset(Original_data,load_ready=False):
+    if load_ready:
+        path = '/home/john/Desktop/Dissertation/Dataset1/Corrupted_1.npy'
+        Variants = np.load(path)
+    else:
+        counts = np.zeros(Original_data.shape[1])
+        Variants = Original_data.copy()
+        loop = tqdm(range(100000))
+        for num in loop:
+            idx =np.random.randint(0,Original_data.shape[1],size=2)
+            counts[idx] += 1
+            i = Original_data[:,idx] #column id
+            a=np.where(i[:,0]!=0)[0] #list of cell 1
+            b= np.where(i[:,1]!=0)[0] #list of cell 2
+            c = random.choice(a)
+            d = random.choice(b)
+            Variants[d,idx[0]] = Original_data[d,idx[1]]
+            Variants[c,idx[0]] = Original_data[c,idx[1]]
+            Variants[c,idx[1]] = Original_data[c,idx[0]] 
+            Variants[d,idx[1]] = Original_data[d,idx[0]]
+        plt.title('Distribution of permutations',fontsize=16)
+        plt.xlabel('Number of changes occured')
+        plt.hist(counts,bins=60)
+        plt.show()
+
+    return Variants
+
+def assign_labels(cells_ID,coeffients,inverse=False):
+    """
+    Fuction assigning labels to cluster
+    Input: coefficients [rank,n_poins]
+    output: dictionart {cell_id : label}
+    """
+    if inverse:
+        ID1 = list(np.where(np.diff(coeffients, axis=0)>0)[1])
+        ID2 = list(np.where(np.diff(coeffients, axis=0)<0)[1])
+    else:
+        ID1 = list(np.where(np.diff(coeffients, axis=0)<0)[1])
+        ID2 = list(np.where(np.diff(coeffients, axis=0)>0)[1])
+    mydict1 = dict.fromkeys(np.array(cells_ID)[ID1], 0)
+    mydict2 = dict.fromkeys(np.array(cells_ID)[ID2], 1)
+    predictions = dict.fromkeys(cells_ID)
+    predictions.update(mydict1)
+    predictions.update(mydict2)
+    return predictions
+
+def cluster_acc(Y_pred, Y):
+  assert Y_pred.size == Y.size , "Sizes do not match"
+  D = max(Y_pred.max(), Y.max())+1
+  w = np.zeros((D,D), dtype=np.int64)
+  for i in range(Y_pred.size):
+    w[Y_pred[i], Y[i]] += 1
+  ind = linear_assignment(w.max() - w)
+  return sum([w[i,j] for i,j in ind])*1.0/Y_pred.size, w
