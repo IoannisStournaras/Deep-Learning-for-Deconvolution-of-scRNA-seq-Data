0f7004405bf65cc57b1b9b56d5dad960a53d02a8
John <giannistou@hotmail.com>
2019-06-24 23:34:14 +0100

    updating files

--- Networks.py
+++ Networks.py
@@ -2,46 +2,45 @@ import torch
 import numpy as np
 from torch import nn, optim
 from torch.nn import functional as F
-from torch.utils import data
 from scipy.special import gamma
-import tqdm
+from tqdm import tqdm
 import json
 from typing import Optional
+import pickle
 
-class Dataset(torch.utils.data.Dataset):
-    def __init__(self, data, labels):
-        self.labels = labels
-        self.data = data
-
-    def __getitem__(self, index):
-        row = self.data[index]
-        row = torch.from_numpy(np.array(row, dtype='float32'))
-        label = self.labels[index]
-        label = torch.from_numpy(np.array(label))
-        return row, label, torch.from_numpy(np.array(index))
-
-    def __len__(self):
-        n, _ = self.data.shape
-        return n
-
-
+def adjust_learning_rate(init_lr, optimizer, epoch):
+    lr = max(init_lr * (0.9 ** (epoch//10)), 0.000001)
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+    return lr
 
 class VAE(nn.Module):
-    def __init__(self,dimensions,rank):
+    def __init__(self, n_input, encode, rank, decode):
         super(VAE, self).__init__()
 
-        self.fcin = nn.Linear(dimensions, 400)
-        self.finter = nn.Linear(400,100)
-        self.L = nn.Linear(100, rank)
-        self.K = nn.Linear(100, rank)
-        self.H = nn.Linear(rank, 100,bias=False)
-        self.fouter = nn.Linear(100, 400,bias=False)
-        self.fcout = nn.Linear(400, dimensions,bias=False)
+        # encoder
+        self.enc_Dims = encode.copy()
+        self.enc_Dims.insert(0,n_input)    
+        self.encode_layers = nn.ModuleList()        
+        for idx in range(len(self.enc_Dims)-1):
+            self.encode_layers.append(nn.Linear(self.enc_Dims[idx],self.enc_Dims[idx+1]))
+        self.L = nn.Linear(self.enc_Dims[-1], rank)
+        self.K = nn.Linear(self.enc_Dims[-1], rank)
+
+        # decoder
+        self.decode_layers = nn.ModuleList()
+        self.dec_Dims = decode.copy() 
+        self.dec_Dims.append(n_input) 
+        self.H = nn.Linear(rank, self.dec_Dims[0],bias=False)
+        for idx in range(len(self.dec_Dims)-1):
+            self.decode_layers.append(nn.Linear(self.dec_Dims[idx],self.dec_Dims[idx+1],bias=False))
         
-    def encode(self, x):
-        h1 = F.relu(self.fcin(x))
-        h2 = F.relu(self.finter(h1))
-        return self.L(h2), self.K(h2)
+    def encode(self, inputs):
+        z = inputs
+        for layers in self.encode_layers:
+            z = layers(z)
+            z = F.relu(z)
+        return self.L(z), self.K(z)
 
     def reparameterize(self, loglamba, logkappa):
         samples = torch.rand_like(loglamba)
@@ -49,15 +48,72 @@ class VAE(nn.Module):
         K = logkappa.exp()
         return L*(-samples.log())**(1/K)
 
-    def decode(self, z):
-        h3 = self.H(z)
-        h4 = self.fouter(h3)
-        return self.fcout(h4)
+    def decode(self, inputs):
+        z = inputs
+        z = self.H(z)
+        for layers in self.decode_layers:
+            z = layers(z)
+        return z
 
     def forward(self, x):
         loglamba, logkappa = self.encode(x)
         z = self.reparameterize(loglamba, logkappa)
-        return self.decode(z), loglamba, logkappa
+        return self.decode(z), loglamba, logkappa, z
+
+    def loss_function(self,approximation, input_matrix, loglamda, logkappa):
+        #Calculation of the Frobenius norm
+        Frobenius = 0.5*(torch.norm(input_matrix-approximation)**2)
+        
+        E = 0.5772 #Euler-Mascheroni constant
+        kappa = logkappa.exp()
+        lamda = loglamda.exp()
+        gamma_input = 1+(1/kappa) 
+        i = torch.lgamma(gamma_input).exp()
+
+        #see PAE-NMF by Steven Squires, Adam Prugel-Bennett, Mahesan Niranjan, ICLR 2019
+        #https://openreview.net/forum?id=BJGjOi09t7
+        #derivation of KL divergence for Weibull distributions
+        KLD = torch.sum(logkappa - kappa*loglamda +(kappa-1)*(loglamda-E/kappa)+lamda*i-1)
+        return Frobenius + KLD
+
+    def fit(self,epochs, train_loader, lr=1e-3, scheduler_act=False, log_interval=23, device='cpu',
+                path='/home/john/Desktop/Dissertation/Dataset3/TrainingError/loss_3HL'):
+        train_loss = []
+        epoch_lr = lr
+        optimizer = optim.Adam(self.parameters(), lr=lr)
+        if scheduler_act:
+            scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma = 0.99)
+        for epoch in range(1, epochs + 1):
+            print("Executing",epoch,"...")  
+            self.train()
+            epoch_loss = 0
+            loop = tqdm(train_loader)
+            for batch_idx, (data, _, _) in enumerate(loop):
+                data = data.to(device)
+                optimizer.zero_grad()
+                recon_batch, loglamba, logkappa, _ = self.forward(data)
+                loss = self.loss_function(recon_batch, data, loglamba, logkappa)
+                loss.backward()
+                epoch_loss += loss.item()
+                optimizer.step()
+                self.H.weight.data.clamp_(0)
+                for layer in self.decode_layers:
+                    layer.weight.data.clamp_(0)
+                if batch_idx % log_interval == 0:
+                    print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                        epoch, batch_idx * len(data), len(train_loader.dataset),
+                        100. * batch_idx / len(train_loader),
+                        loss.item() / len(data)))
+            if (epoch>50 and scheduler_act):            
+                scheduler.step()
+            else:
+                epoch_lr = adjust_learning_rate(lr, optimizer, epoch)
+            aver_loss = epoch_loss / len(train_loader.dataset)
+            train_loss.append(aver_loss)
+            print('====> Epoch: {} Average loss: {:.4f}, Learning rate: {}'.format(
+                epoch, aver_loss, epoch_lr))
+        with open(path, 'wb') as f:
+            pickle.dump(train_loss, f)
 
 class AE(nn.Module):
 
--- run_VAE_NMF.py
+++ run_VAE_NMF.py
@@ -4,7 +4,7 @@ import torch
 from torch import nn, optim
 from torch.nn import functional as F
 from torch.utils import data
-from Networks import VAE
+from PAE-NMF import VAE
 from Datasets import Dataset
 from BMF import BMF
 from tqdm import tqdm
@@ -17,6 +17,8 @@ parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                     help='input batch size for training (default: 128)')
 parser.add_argument('--epochs', type=int, default=10, metavar='N',
                     help='number of epochs to train (default: 10)')
+parser.add_argument('--lr', type=float, default=0.001, metavar='N',
+                    help='learning rate for training (default: 0.001)')
 parser.add_argument('--cuda', action='store_false', default=True,
                     help='enables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
@@ -37,16 +39,14 @@ device = torch.device("cuda" if args.cuda else "cpu")
 kwargs = {'num_workers': 8, 'pin_memory': True} if args.cuda else {}
 
 print('Loading the dataset...')
-path = '/home/john/Desktop/Dissertation/data/labels_PCA'
+path = '/home/john/Desktop/Dissertation/Dataset1/labels_PCA'
 with open(path, 'rb') as f:
     labels_dict = pickle.load(f)
 labels_ID = list(labels_dict.keys())
 
-path = '/home/john/Desktop/Dissertation/data/Corrupted_1.npy'
-df_train = np.load(path)
-df_train = df_train.T
+path = '/home/john/Desktop/Dissertation/Dataset1/Dataset_1.npy'
+df_train = np.load(path) 
 labels = np.array(list(labels_dict.values()))
-
 print('Creating DataLoader...')
 train_dataset = Dataset(data=df_train,labels=labels)
 train_loader = torch.utils.data.DataLoader(train_dataset,
@@ -57,64 +57,12 @@ train_loader = torch.utils.data.DataLoader(train_dataset,
 print('Created the DataLoader')
 
 #Initializing model-optimizer-scheduler
-model = VAE(df_train.shape[1],args.embeddings).to(device)
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
-scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma = 0.99)
-
-def loss_function(approximation, input_matrix, loglamda, logkappa):
-    #Calculation of the Frobenius norm
-    Frobenius = 0.5*(torch.norm(input_matrix-approximation)**2)
-    
-    E = 0.5772 #Euler-Mascheroni constant
-    kappa = logkappa.exp()
-    lamda = loglamda.exp()
-    gamma_input = 1+(1/kappa) 
-    i = torch.lgamma(gamma_input).exp()
-
-    #see PAE-NMF by Steven Squires, Adam Prugel-Bennett, Mahesan Niranjan, ICLR 2019
-    #https://openreview.net/forum?id=BJGjOi09t7
-    #derivation of KL divergence for Weibull distributions
-    KLD = torch.sum(logkappa - kappa*loglamda +(kappa-1)*(loglamda-E/kappa)+lamda*i-1)
-    return Frobenius + KLD
-    
-def train(epoch):
-    model.train()
-    train_loss = 0
-    loop = tqdm(train_loader)
-    for batch_idx, (data, _, _) in enumerate(loop):
-        data = data.to(device)
-        optimizer.zero_grad()
-        recon_batch, loglamba, logkappa, _ = model(data)
-        loss = loss_function(recon_batch, data, loglamba, logkappa)
-        loss.backward()
-        train_loss += loss.item()
-        optimizer.step()
-        model.fcout.weight.data.clamp_(0)
-        #model.fouter.weight.data.clamp_(0)
-        model.H.weight.data.clamp_(0)
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader),
-                loss.item() / len(data)))
-    if epoch>50:            
-        scheduler.step()
-    aver_loss = train_loss / len(train_loader.dataset)
-    print('====> Epoch: {} Average loss: {:.4f}'.format(
-          epoch, aver_loss))
-    return aver_loss
-
+model = VAE(df_train.shape[1],[400,100],args.embeddings,[100,400]).to(device)
+print(model)
 
 if __name__ == "__main__":
-    train_loss = []
-    for epoch in range(1, args.epochs + 1):
-        print("Executing",epoch,"...")
-        train_loss.append(train(epoch))
-        
-    path = '/home/john/Desktop/Dissertation/TrainingError/loss_2HL_corrupted'
-    with open(path, 'wb') as f:
-        pickle.dump(train_loss, f)
-    path_to_save_params = '/home/john/Desktop/Dissertation/Pretrained Weights/2HL_corrupted'
+    model.fit(epochs=args.epochs, train_loader = train_loader, lr = args.lr, device=device)
+    path_to_save_params = '/home/john/Desktop/Dissertation/Dataset3/Pretrained Weights/3HL'
     torch.save(model.state_dict(), path_to_save_params)
 
     print('Executing Binary Matrix Factorization...')
@@ -122,16 +70,16 @@ if __name__ == "__main__":
     row = torch.from_numpy(np.array(df_train, dtype='float32'))
     row = row.to('cpu')
     recon_batch, loglamba, logkappa, H = model(row)
-    third = model.fcout.weight.detach().numpy()
-    second = model.fouter.weight.detach().numpy()
-    first = model.H.weight.detach().numpy()
-    W_learned = third@second@first
+    W_learned = model.H.weight.detach().numpy()
+    for layer in model.decode_layers:
+        Wi = layer.weight.detach().numpy()
+        W_learned = Wi@W_learned
     H_learned = H.detach().numpy().T
 
     model_1 = BMF(W_init=W_learned,H_init=H_learned,tol=1e-5)
     W_final, H_final = model_1.fit_transform(df_train.T)
-    path = '/home/john/Desktop/Dissertation/Decompositions/3HL_BMF_basis.npy'
+    path = '/home/john/Desktop/Dissertation//Dataset3/Decompositions/3HL_BMF_basis.npy'
     np.save(path, W_final)
-    path = '/home/john/Desktop/Dissertation/Decompositions/3HL_BMF_coeff.npy'
+    path = '/home/john/Desktop/Dissertation//Dataset3/Decompositions/3HL_BMF_coeff.npy'
     np.save(path, H_final)
 
